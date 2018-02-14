bdb93e6b7afe50b4ebdc45bebe14a6b56877b5bb
splnlss <sans.spleen@gmail.com>
2018-02-14 09:54:12 -0500

    added basic server response test

--- server.js
+++ server.js
@@ -1,4 +1,62 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const {
+  DATABASE_URL, PORT
+} = require('./config');
+
+// require models
+
 const app = express();
+
+app.use(morgan('common'));
+app.use(bodyParser.json());
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+
+app.get('/', (req, res) => {
+  return res.status(200).send(message);
+})
+
+let server
+
+function runServer(databaseURL = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseURL, err => {
+      if (err) {
+        return reject(err)
+      }
+      server = app.listen(port, () => {
+          console.log(`You're app is listening on ${port}`)
+          resolve()
+        })
+        .on('error', err => {
+          mongoose.disconnect()
+          reject(err)
+        })
+    })
+  })
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('closing server')
+      server.close(err => {
+        if (err) {
+          return reject(err)
+        }
+        resolve()
+      })
+    })
+  })
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err))
+}
+
+module.exports = (runServer, app, closeServer)
---
+++ test/test-ping-server.js
@@ -0,0 +1,23 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const mongoose = require('mongoose')
+
+const should = chai.should();
+
+// const { SeatReview } = require('../models');
+const { closeServer, runServer, app } = require('../server');
+const { TEST_DATABASE_URL } = require('../config');
+
+chai.use(chaiHttp);
+
+describe('GET endpoint', function () {
+  it('should return server 200 msg', function () {
+    let resolve
+    return chai.request(app)
+      .get('/')
+      .then(_res =>{
+        res = _ res
+        res.should.have.status(200)
+      })
+  })
+})
