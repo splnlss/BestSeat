7556893a0a5771659001f6c788883304835787da
splnlss <sans.spleen@gmail.com>
2018-02-16 10:42:52 -0500

    added chair endpoint test

--- test/test-ping-server.js
+++ test/test-ping-server.js
@@ -6,18 +6,32 @@ const should = chai.should();
 
 // const { SeatReview } = require('../models');
 // const { closeServer, runServer, app } = require('../server');
-const { TEST_DATABASE_URL } = require('../config');
+const {
+  TEST_DATABASE_URL
+} = require('../config');
 
 const app = require('../server')
 
 chai.use(chaiHttp);
 
-describe('GET endpoint', function () {
-  it('should return server 200 msg', function () {
+describe('GET endpoint', function() {
+  it('should return server 200 msg', function() {
     let resolve
     return chai.request(app)
       .get('/')
-      .then(_res =>{
+      .then(_res => {
+        res = _res
+        res.should.have.status(200)
+      })
+  })
+})
+
+describe('Get chair.html endpoint', function() {
+  it('shoud return server 200', function() {
+    let resolve
+    return chai.request(app)
+      .get('/chair.html')
+      .then(_res => {
         res = _res
         res.should.have.status(200)
       })
