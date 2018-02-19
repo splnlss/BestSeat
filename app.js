dfc2d0f5a811beb8b9ae0b038ada21341995e207
splnlss <sans.spleen@gmail.com>
2018-02-19 08:44:50 -0500

    added mockdata to build out api schema

---
+++ app.js
@@ -0,0 +1,47 @@
+let MOCK_NEW_REVIEWS = {
+    'newReviews': [{
+      "id": "1111111",
+      "venue": "McDonalds",
+      "chairReview": "Hard, plastic. The worst for a bad tailbone",
+      "userName": "John Doe",
+      "publishedAt": 1470016976609
+    }, {
+      "id": "2222222",
+      "venue": "Cookshop",
+      "chairReview": "Wooden chair - decently comfortable for about 30 min",
+      "userName": "Jane Doe",
+      "publishedAt": 1470012976609
+    }, {
+      "id": "333333",
+      "venue": "Glasserie",
+      "chairReview": "Folding yard chairs - How did these ever get popular in restaurants? Zero lumbar support, tailbone protrudes. You just want to smash them. ",
+      "userName": "Jim Doe",
+      "publishedAt": 1470011976609
+    }, {
+      "id": "4444444",
+      "venue": "Buttermilk Channel",
+      "chairReview": "Folding yard chairs AGAIN",
+      "userName": "Jackie Doe",
+      "publishedAt": 1470009976609
+    }]
+
+    function getNewReviews(callbackFn) {
+      setTimeout(function() {
+        callbackFn(MOCK_NEW_REVIEWS)
+      }, 100);
+    }
+
+    function displayNewReviews(data) {
+      for (index in data.newReviews) {
+        $('body').append(
+          '<p>' + data.newReviews[index].text + '</p>');
+      }
+    }
+
+    function getAndDisplayStatusUpdates() {
+      getRecentStatusUpdates(displayStatusUpdates);
+    }
+
+    $(function() {
+      getAndDisplayNewReviews();
+    })
