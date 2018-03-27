d526000fb9f9fb52307e579f352e383a65405cd7
splnlss <jon@splnlss.com>
2018-03-26 23:35:32 -0400

    change DATABASE_URL to direct link

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-//remove _DIRECT to pull from Heroku settings instead
-DATABASE_URL_DIRECT = "mongodb://admin:password27@ds147228.mlab.com:47228/bestseat?authSource=admin"
+//remove direct link to pull from Heroku settings instead
 
-exports.DATABASE_URL = process.env.DATABASE_URL_DIRECT ||
-  'mongodb://localhost/bestseat-app'
+exports.DATABASE_URL = "mongodb://admin:password27@ds147228.mlab.com:47228/bestseat?authSource=admin"
+//process.env.DATABASE_URL || 'mongodb://localhost/bestseat-app'
+
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
   'mongodb://localhost/test-bestseat-app'
 exports.PORT = process.env.PORT || 8080
