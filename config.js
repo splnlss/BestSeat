20337a14218eb6255fa1d6210b5fa488c8612d1c
splnlss <jon@splnlss.com>
2018-03-26 23:45:27 -0400

    reverted back to normal DATABASE_URL

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-//remove direct link to pull from Heroku settings instead
-
-exports.DATABASE_URL = "mongodb://admin:password27@ds147228.mlab.com:47228/bestseat"
-//process.env.DATABASE_URL || 'mongodb://localhost/bestseat-app'
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/bestseat-app'
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
   'mongodb://localhost/test-bestseat-app'
@@ -9,5 +6,5 @@ exports.PORT = process.env.PORT || 8080
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret'
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'
 
-console.log(exports.DATABASE_URL)
+console.log(`DATABASE_URL = ${exports.DATABASE_URL}`)
 // should be in config heroku
