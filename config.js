91b3af510dd0b7bcb36395d3a3d6b890bc69154c
splnlss <splnlss@Splnlss-MBP.T-mobile.com>
2018-03-26 22:55:57 -0400

    added console.log to config.js

--- config.js
+++ config.js
@@ -6,4 +6,5 @@ exports.PORT = process.env.PORT || 8080
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret'
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'
 
+console.log(exports.DATABASE_URL)
 // should be in config heroku
