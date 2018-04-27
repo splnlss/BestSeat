763e268ec0ba13b861a05afc740055f905cc3530
splnlss <jon@splnlss.com>
2018-04-27 12:23:21 -0400

    removed boostrap

--- public/app.js
+++ public/app.js
@@ -13,11 +13,11 @@ function getAllReviews (success, failure){
       }else{
         failure()
       }
-    }
+    },
+    failure
   }
   if (jwt){
     settings.headers = { Authorization: `Bearer ${jwt}` }
-
   }
   $.ajax(settings)
 }
@@ -47,7 +47,8 @@ function searchAndGetAllReviews (success, failure, searchTerm){
       }else{
         failure()
       }
-    }
+    },
+    failure
   }
   if (jwt){
     settings.headers = { Authorization: `Bearer ${jwt}` }
@@ -56,16 +57,14 @@ function searchAndGetAllReviews (success, failure, searchTerm){
 }
 
 function postReview (review, success, failure){
+  console.log(review)
   const settings = {
     url: '/api/review',
     type: 'POST',
     data: JSON.stringify(review),
     dataType: 'json',
     contentType: 'application/json',
-    success:function(data){
-      console.log(review)
-      getAndDisplayNewReviews()
-    },failure
+    success,failure
   }
   if (jwt){
   settings.headers = { Authorization: `Bearer ${jwt}` }
@@ -75,15 +74,14 @@ function postReview (review, success, failure){
 
 function putReview (review, success, failure){
   console.log('put review')
+  console.log(review)
   const settings = {
     url: `/api/review/${review.id}`,
     type: 'PUT',
     data: JSON.stringify(review),
     dataType: 'json',
     contentType: 'application/json',
-    success: function(data){
-      console.log(data)
-    },
+    success,
     failure
   }
   if (jwt){
@@ -105,39 +103,6 @@ function deleteReview (id, success, failure){
   $.ajax(settings)
 }
 
-// Yelp Search
-function searchYelp (searchTerms, success, error){
-  console.log(searchRequest)
-
-  const settings = {
-
-    url:`/api/yelp`,
-    type: 'GET',
-    data: searchRequest,
-    dataType: 'json',
-    success,error
-  }
-  $.ajax(settings)
-}
-
-const searchRequest = {
-  term:'Joe Coffee',
-  location: '13th street, New York'
-};
-
-function displaySearchYelp(){
-      searchYelp(searchRequest,function (data,text){
-       console.log(arguments)
-        console.log(data,text)
-    })
-      // console.log(data)
-}
-
-
-// function searchRequest (searchVenue,searchLocation){
-//
-// }
-
 // APP FUNCTIONS
 
 function displayHeader(){
@@ -147,62 +112,29 @@ function displayHeader(){
 }
 
 function renderHeader(){
-  // const titleLink = `<a id="title">Best Seat In The House</a>`
-  const addLink = `<a class="nav-link" href="#" id="addForm">Add</a>`
-  const logInLink = `<a class="nav-link" href="#" id="loginForm">Login</a>`
-  const newUserLink = `<a class="nav-link" href="#" id="newUserForm">New User</a>`
-  const searchLink = `<a class="nav-link" href="#" id="searchForm">Search</a>`
-  const logOutLink = `<a class="nav-link" href="#" id="logOut">Logout</a>`
-  const loggedIn = [addLink, logOutLink, searchLink]
-  const loggedOut = [logInLink, newUserLink, searchLink]
+  const titleLink = `<a id="title">Best Seat In The House</a>`
+  const addLink = `<a id="addForm">Add</a>`
+  const logInLink = `<a id="loginForm">Login</a>`
+  const newUserLink = `<a id="newUserForm">New User</a>`
+  const searchLink = `<a id="searchForm">Search</a>`
+  const logOutLink = `<a id="logOut">Logout</a>`
+  const loggedIn = [addLink, searchLink, logOutLink]
+  const loggedOut = [searchLink, logInLink, newUserLink]
   const links = jwt?loggedIn:loggedOut
 
-  const navLinks = function(){
-        links.forEach(function(link){
-          `<li class="nav-item">
-          ${link}
-          </li>`
-          return link
-          // return links.join("; ")
-        })
-        return `${links.join("")}`
-      }
-
-  const navBar = `<nav class="navbar fixed-top navbar-light navbar-expand-lg">
-    <a class="navbar-brand" id="title" href="#">Best Seat In The House </a>
-    <button class="navbar-toggler " type="button" data-toggle="dropdown" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse dropdown-menu show dropdown-menu-right" id="navbarNav">
-      <ul class="navbar-nav">
-      ${navLinks()}
-      </ul>
-    </div>
-  </nav>`
-  // <h1>${titleLink}</h1>
-  // <nav>${links.join(` / `)}</nav>
-  // `
-  return navBar
+  return `<h1>${titleLink}</h1>
+  <nav>${links.join(` / `)}</nav>`
 }
 
 //changed from button to a link
 function renderReview(review) {
+console.log(review)
 const editReview = `<a class="editReview" data-reviewid=${review.id}>Edit</a>
   <a class="deleteReview" data-reviewid=${review.id}>Delete</a>`
-  return `<li><ul>
-  <div class="container">
-    <div class="row">
-      <div class="col-4">
-        <img class="reviewIMG" src="${review.imageURL}" alt="venue image">
-      </div>
-      <div class="col-8">
-        <ul><h2>${review.venue}</h2></ul>
-        <ul><span>${review.chairReview}</span></ul>
-        <ul>${jwt?editReview:""}</ul>
-      </div>
-    </div>
-  </div>
-  </ul>
+  return `<img src="${review.imageURL}" height="100px" alt="Chair Review Image">
+  <li><ul><h3>${review.venue}</h3></ul>
+  <ul><span>${review.chairReview}</span></ul>
+  <ul>${jwt?editReview:""}</ul>
   </li>
   <svg width="250" height="1" viewBox="0 0 300 1"
     xmlns="http://www.w3.org/2000/svg">
@@ -217,7 +149,7 @@ function renderReviews(reviews) {
 }
 
 function displayNewReviews(data) {
-
+  console.log(data)
   $('main').html(
     renderReviews(data)//NEED TO SORT BY DATE!!
   )
@@ -226,11 +158,12 @@ function displayNewReviews(data) {
 function getAndDisplayNewReviews() {
   displayHeader()
   getAllReviews(displayNewReviews, noReviews)
-  setupUIHandlers()
 }
 
 function searchAndDisplayNewReviews(searchTerm){
+  displayHeader()
   searchAndGetAllReviews(displayNewReviews, noSearchResults, searchTerm)
+
 }
 
 function renderReviewForm(review) {
@@ -240,12 +173,9 @@ function renderReviewForm(review) {
   <form id="${review?'chairEditForm':
     'chairAddForm'}"${review?reviewDataID:""}>
     <div><label for="venue">Venue:</label>
-    <input type="text" id="venueInput" name="venue" placeholder="Enter venue name"> </input></div>
+    <input type="text" id="venueInput" name="venue"> </input></div>
     <div><label for="review">Chair Review:</label>
-    <input type="text" id="reviewInput" name="chairReview" placeholder="Enter review"> </input>
-    </div>
-    <div><label for="imageURL">Add image URL</label>
-    <input type="url" id="imageInput" name="imageURL" placeholder="http://domain.com/image.jpg"> </input>
+    <input type="text" id="reviewInput" name="chairReview"> </input>
     </div>
     <div id="formButtons">
       <input type="button" id="cancel" value="cancel"></input>
@@ -264,14 +194,14 @@ function displayEditForm(review) {
   $('main').html(renderReviewForm(review))
   $('#venueInput').val(review.venue)
   $('#reviewInput').val(review.chairReview)
-  $('#imageInput').val(review.imageURL)
+  $('#userNameInput').val(review.userName)
 }
 
 function renderSearchForm() {
-  return ` <h2>Search By Venue</h2>
+  return ` <h3>Search By Venue</h3>
   	<form id="chairSearchForm">
     <label for="venue">Venue:</label>
-    <input type="text" id="venueSearch" name="venueSearch" placeholder="Enter venue name"> </input>
+    <input type="text" id="venueSearch" name="venueSearch"> </input>
     <div>
       <input type="button" id="cancel" value="cancel"></input>
       <input type="submit" id="submit" value="search"></input>
@@ -311,7 +241,7 @@ function handleAddFormSubmit(event) {
   const review = {
     venue : $('#venueInput').val(),
     chairReview : $('#reviewInput').val(),
-    imageURL: $('#imageInput').val()
+    userName : $('#userNameInput').val()
   }
   postReview(review, getAndDisplayNewReviews, handleApiError)
 }
@@ -323,9 +253,8 @@ function handleEditFormSubmit(event){
     id:reviewID,
     venue : $('#venueInput').val(),
     chairReview : $('#reviewInput').val(),
-    imageURL: $('#imageInput').val()
+    userName : $('#userNameInput').val()
   }
-  console.log(review)
   putReview(review, getAndDisplayNewReviews, handleApiError)
 }
 
@@ -347,7 +276,6 @@ function handleApiError(err){
 
 function displayLoginForm(){
   $('main').html(
-    // $('.login-form').toggle();
     renderLoginForm()
   )}
 
@@ -358,28 +286,25 @@ function displayNewUserForm(){
 //
 function renderLoginForm(){
   return `<form id="userLogin">
-    <h2>Login</h2>
+    <h3>Login</h3>
     <div>
-      <label for="userName">Username:</label>
-      <input type="text" id="userNameInput" name="userName" placeholder="Enter username"> </input>
-    </div>
+    <label for="userName">Username:</label>
+    <input type="text" id="userNameInput" name="userName"> </input></div>
+    <div><label for="userPassword">Password: </label>
+    <input type="text" id="userPasswordInput" name="userPassword"></input></div>
     <div>
-      <label for="userPassword">Password:</label>
-      <input type="password" id="userPasswordInput" name="userPassword" placeholder="Enter password"></input>
-    </div>
-    <div>
-      <input type="button" id="cancel" value="cancel"></input>
-      <input type="submit" id="submit" value="login"></input>
+    <input type="button" id="cancel" value="cancel"></input>
+    <input type="submit" id="submit" value="login"></input>
     </div>
   </form>`
 }
 function renderNewUserForm(){
   return `<form id="newUserLogin">
-    <div><h2>Create New User</h2></div>
+    <div><h3>Create New User</h3></div>
     <label for="userName">Username:</label>
-    <input type="text" id="userNameInput" name="userName" placeholder="Enter username"> </input><br>
+    <input type="text" id="userNameInput" name="userName"> </input><br>
     <label for="userPassword">Password: </label>
-    <input type="text" id="userPasswordInput" name="userPassword" placeholder="Enter password"></input>
+    <input type="text" id="userPasswordInput" name="userPassword"></input>
     <div>
       <input type="button" id="cancel" value="cancel"></input>
       <input type="submit" id="submit" value="create user"></input>
@@ -388,6 +313,7 @@ function renderNewUserForm(){
 }
 
 function postUserLogin(userData, success, failure){
+  console.log(userData)
   const settings = {
     url: '/api/auth/login',
     type: 'POST',
@@ -400,20 +326,10 @@ function postUserLogin(userData, success, failure){
     //  console.log(atob(jwt))
       success(data)
     },
-//why is failure being called with succes as well??
-    failure: userNotFound()
+    failure
   }
   $.ajax(settings)
 }
-
-function userNotFound(){
-  console.log('posting user error')
-  $('main').append(`
-    <section role="region" id="instructions" aria-live="assertive">
-      <span>User or password not found. Please check spelling and try again.</span>
-    </section>
-    `)
-  }
 
 function postNewUserLogin(userData, success, failure){
   console.log(userData)
@@ -424,6 +340,7 @@ function postNewUserLogin(userData, success, failure){
     dataType: 'json',
     contentType: 'application/json',
     success: function(data){
+      //run user.create
       success(data)
     },
     failure
@@ -456,18 +373,42 @@ function logOutUser(){
   console.log('logged out')
 }
 
+// Yelp Search
+function searchYelp (searchTerms, success, error){
+  console.log(searchRequest)
+
+  const settings = {
+
+    url:`/api/yelp`,
+    type: 'GET',
+    data: searchRequest,
+    dataType: 'json',
+    success,error
+  }
+  $.ajax(settings)
+}
+
+const searchRequest = {
+  term:'Joe Coffee',
+  location: 'New York, NY'
+};
+
+function displaySearchYelp(){
+      searchYelp(searchRequest,function (data,text){
+       console.log(arguments)
+        console.log(data,text)
+    })
+      // console.log(data)
+}
+
 //EVENT HANDLERS
 function setupUIHandlers() {
-  $('nav').on('click', '#addForm', displayAddForm)
-  $('nav').on('click', '#loginForm', displayLoginForm)
-  $('nav').on('click', '#newUserForm', displayNewUserForm)
-  $('nav').on('click', '#title', getAndDisplayNewReviews)
-  $('nav').on('click', '#logOut', logOutUser)
-  $('nav').on('click', '#searchForm', displaySearchYelp)
-  // displaySearchForm)
-  $('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide')
-    })
+  $('header').on('click', '#addForm', displayAddForm)
+  $('header').on('click', '#loginForm', displayLoginForm)
+  $('header').on('click', '#newUserForm', displayNewUserForm)
+  $('header').on('click', '#title', getAndDisplayNewReviews)
+  $('header').on('click', '#logOut', logOutUser)
+  $('header').on('click', '#searchForm', displaySearchForm)
   $('main').on('submit', '#userLogin', handleUserLoginSubmit)
   $('main').on('submit', '#newUserLogin', handleNewUserLoginSubmit)
   $('main').on('submit', '#chairEditForm', handleEditFormSubmit)
@@ -482,7 +423,6 @@ function setupUIHandlers() {
 //Fire up page
 
 $(function() {
-  //displayHeader()
   getAndDisplayNewReviews()
-//  setupUIHandlers()
+  setupUIHandlers()
 })
